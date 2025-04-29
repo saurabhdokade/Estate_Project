6faf078c30c8e1b85326392202902c358d0516cf
saurabh eknath dokade <saurabhdokade77@gmail.com>
2025-04-29 18:55:01 +0530

    .

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 //   })
 // })
 // Start the Server
-const PORT = process.env.PORT ||  5000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
